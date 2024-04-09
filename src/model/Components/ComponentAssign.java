922c9e98da2871ccd00a3143caa7a207e2cae9b8
Effeemme06 <effeemmeco6@gmail.com>
2024-04-09 10:47:39 +0200

    Memory on General component

--- src/model/Components/ComponentAssign.java
+++ src/model/Components/ComponentAssign.java
@@ -4,19 +4,19 @@ import model.Memory.Variable;
 
 public class ComponentAssign extends AlgorithmComponent{
 
-	private String finalVariableName;
-	private String variable1Name;
-	private String variable2Name;
 	private String operation;
 	
 	private Variable finalVariable;
 	private Variable variable1;
 	private Variable variable2;
-	
-	 
-	
-	public ComponentAssign(AlgorithmComponent nextComponent1, AlgorithmComponent nextComponent2) {
+
+	public ComponentAssign(AlgorithmComponent nextComponent1, AlgorithmComponent nextComponent2,
+			String finalVariableName, String variable1Name, String variable2Name, String operation) {
 		super(nextComponent1, nextComponent2);
+		this.finalVariable = finalVariableName;
+		this.variable1 = variable1Name;
+		this.variable2 = variable2Name;
+		this.operation = operation;
 	}
 	
 	
