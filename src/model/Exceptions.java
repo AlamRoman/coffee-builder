31e9d257bc87e59d00d65412394e1df777e4e911
Effeemme06 <effeemmeco6@gmail.com>
2024-04-09 12:04:38 +0200

    Others changes

--- src/model/Exceptions.java
+++ src/model/Exceptions.java
@@ -5,6 +5,7 @@ public class Exceptions extends Exception{
 	public static final String COMPONENT_NOT_SET = "Set the component before executing the algorithm";
 	public static final String EXISTING_VARIABLE = "The variable already exists, choose another name";
 	public static final String NON_EXISTING_VARIABLE = "The variable not exists, try whit another name";
+	public static final String UNMATCH_TYPE = "The variable type and the type of the value aren't the same!";
 
 	public Exceptions(String s) {
 		super(s);
