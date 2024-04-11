26ea4f810e44f0977aef24c11c7fa92e711850f9
Roman <romanalam8083@gmail.com>
2024-04-11 10:49:51 +0200

    update editComponent

--- src/view/editComponents/EditComment.java
+++ src/view/editComponents/EditComment.java
@@ -2,4 +2,7 @@ package view.editComponents;
 
 public class EditComment extends EditComponent{
 
+	public EditComment(String componentName) {
+	}
+
 }
--- src/view/editComponents/EditComponent.java
+++ src/view/editComponents/EditComponent.java
@@ -1,5 +1,20 @@
 package view.editComponents;
 
-public class EditComponent{
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
+public class EditComponent extends JFrame{
+
+	private JPanel contentPane;
+	private String componentName;
+	
+	public EditComponent() {
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(200, 250);
+		setTitle(componentName);
+		
+		contentPane = new JPanel();
+		
+		setContentPane(contentPane);
+	}
 }
