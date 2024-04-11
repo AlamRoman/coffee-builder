35d152a960ec9bcb493da88d13b504c478ece060
Effeemme06 <effeemmeco6@gmail.com>
2024-04-11 16:30:23 +0200

    Frame Main

--- src/view/Frame.java
+++ src/view/Frame.java
@@ -1,10 +1,14 @@
 package view;
 
 import java.awt.EventQueue;
+import java.awt.Toolkit;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import model.Components.ComponentDeclaration;
+import model.Components.ComponentEnd;
 
 public class Frame extends JFrame {
 
@@ -31,12 +35,19 @@ public class Frame extends JFrame {
 	 * Create the frame.
 	 */
 	public Frame() {
+		setTitle("Coffee builder");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
+	    setExtendedState(JFrame.MAXIMIZED_BOTH);
+		contentPane = new Panel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 	}
+	
+	public static void main1(String[] args) {
+		
+		Frame ciao = new Frame();
+		}
 
 }
--- src/view/Panel.java
+++ src/view/Panel.java
@@ -2,10 +2,18 @@ package view;
 
 import javax.swing.JPanel;
 import javax.swing.BoxLayout;
+import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import java.awt.BorderLayout;
 import javax.swing.JToolBar;
 import javax.swing.JTextArea;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
 
 public class Panel extends JPanel {
 
@@ -16,6 +24,71 @@ public class Panel extends JPanel {
 	 */
 	public Panel() {
 
+		this.setMaximumSize(getMaximumSize());
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{347, 0, 0};
+		gridBagLayout.rowHeights = new int[]{32, 0, 71, -36, 0, 0};
+		gridBagLayout.columnWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 1.0, 0.0, 1.0, Double.MIN_VALUE};
+		setLayout(gridBagLayout);
+		
+		JButton avviaButton = new JButton("avvia");
+		avviaButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		GridBagConstraints gbc_avviaButton = new GridBagConstraints();
+		gbc_avviaButton.insets = new Insets(0, 0, 5, 5);
+		gbc_avviaButton.gridx = 0;
+		gbc_avviaButton.gridy = 0;
+		add(avviaButton, gbc_avviaButton);
+		
+		JLabel lblNewLabel = new JLabel("Debugger");
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 0);
+		gbc_lblNewLabel.gridx = 1;
+		gbc_lblNewLabel.gridy = 1;
+		add(lblNewLabel, gbc_lblNewLabel);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+		gbc_scrollPane.gridheight = 4;
+		gbc_scrollPane.insets = new Insets(0, 0, 0, 5);
+		gbc_scrollPane.fill = GridBagConstraints.BOTH;
+		gbc_scrollPane.gridx = 0;
+		gbc_scrollPane.gridy = 1;
+		add(scrollPane, gbc_scrollPane);
+		
+		JScrollPane scrollPane_2 = new JScrollPane();
+		GridBagConstraints gbc_scrollPane_2 = new GridBagConstraints();
+		gbc_scrollPane_2.insets = new Insets(0, 0, 5, 0);
+		gbc_scrollPane_2.fill = GridBagConstraints.BOTH;
+		gbc_scrollPane_2.gridx = 1;
+		gbc_scrollPane_2.gridy = 2;
+		add(scrollPane_2, gbc_scrollPane_2);
+		
+		JTextArea debuggerText = new JTextArea();
+		debuggerText.setEditable(false);
+		scrollPane_2.setViewportView(debuggerText);
+		
+		JLabel lblNewLabel_1 = new JLabel("Output");
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 0);
+		gbc_lblNewLabel_1.gridx = 1;
+		gbc_lblNewLabel_1.gridy = 3;
+		add(lblNewLabel_1, gbc_lblNewLabel_1);
+		
+		JScrollPane scrollPane_1 = new JScrollPane();
+		GridBagConstraints gbc_scrollPane_1 = new GridBagConstraints();
+		gbc_scrollPane_1.fill = GridBagConstraints.BOTH;
+		gbc_scrollPane_1.gridx = 1;
+		gbc_scrollPane_1.gridy = 4;
+		add(scrollPane_1, gbc_scrollPane_1);
+		
+		JTextArea outputArea = new JTextArea();
+		outputArea.setEditable(false);
+		scrollPane_1.setViewportView(outputArea);
+		
 	}
 
 }
