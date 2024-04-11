1c8f58efc43b312642254a3b3a394e62f59cb61d
Effeemme06 <effeemmeco6@gmail.com>
2024-04-11 10:06:14 +0200

    Update Panel.java

--- src/view/Panel.java
+++ src/view/Panel.java
@@ -1,6 +1,11 @@
 package view;
 
 import javax.swing.JPanel;
+import javax.swing.BoxLayout;
+import javax.swing.JScrollPane;
+import java.awt.BorderLayout;
+import javax.swing.JToolBar;
+import javax.swing.JTextArea;
 
 public class Panel extends JPanel {
 
@@ -10,6 +15,16 @@ public class Panel extends JPanel {
 	 * Create the panel.
 	 */
 	public Panel() {
+		setLayout(new BorderLayout(0, 0));
+		
+		JToolBar toolBar = new JToolBar();
+		add(toolBar, BorderLayout.NORTH);
+		
+		JPanel panel = new JPanel();
+		add(panel, BorderLayout.SOUTH);
+		
+		JTextArea textArea = new JTextArea();
+		panel.add(textArea);
 
 	}
 
