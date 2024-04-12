758cf3d38b54a42a3b7e3c0a14b7c3a7ceee506e
Effeemme06 <effeemmeco6@gmail.com>
2024-04-12 16:56:07 +0200

    Update ComponentWhile.java

--- src/model/Components/ComponentWhile.java
+++ src/model/Components/ComponentWhile.java
@@ -1,5 +1,43 @@
 package model.Components;
 
+import model.Exceptions;
+import model.Memory.MemoryStorage;
+import model.Memory.RelationalOperators;
+
 public class ComponentWhile extends Component{
+	
+	private String term1;
+	private String term2;
+	private RelationalOperators operand;
+	private Component nextComponent;
+	private Condition C;
+
+	public ComponentWhile(Component nextComponent1, Component nextComponent2, MemoryStorage memory) {
+		super(nextComponent1, nextComponent2, memory);
+		// TODO Auto-generated constructor stub
+		
+	}
+	
+	public void set(String term1, String term2, RelationalOperators operand) {
+		this.term1 = term1;
+		this.term2 = term2;
+		this.operand = operand; 
+		this.C = new Condition(getMemory(), term1, operand.symbol, term2);
+	}
+	
+	@Override
+	public Object execute() throws Exceptions {
+		if(C.resolve()) {
+			nextComponent = getNextComponent1();
+		}else {
+			nextComponent = getNextComponent2();
+		}
+		return null;
+	}
+	
+	@Override
+	public Component getNextComponent() {
+		return nextComponent;
+	}
 
 }
