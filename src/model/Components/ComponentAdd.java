9cf2838d9913fba4da773e65624a6ae536f68f79
Sossics <michelerusso267@gmail.com>
2024-04-15 08:00:37 +0200

    Added constructor

--- src/model/Components/ComponentAdd.java
+++ src/model/Components/ComponentAdd.java
@@ -1,7 +1,13 @@
 package model.Components;
 
 import model.AlgorithmExecuter;
+import model.Memory.MemoryStorage;
 
-public class ComponentAdd extends AlgorithmExecuter{
+public class ComponentAdd extends Component {
+
+	public ComponentAdd() {
+		super(null, null, null);
+		// TODO Auto-generated constructor stub
+	}
 	
 }
