91ad04f4ccb7ecece947e232cb8e08db980dff9a
Roman <romanalam8083@gmail.com>
2024-04-16 12:15:19 +0200

    Update FlowChartContentPanel.java

--- src/view/FlowChartContentPanel.java
+++ src/view/FlowChartContentPanel.java
@@ -3,24 +3,14 @@ package view;
 import javax.swing.JPanel;
 
 import model.Components.ComponentEnd;
-import model.Components.ComponentInput;
-import model.Components.ComponentOperation;
 import model.Components.ComponentOutput;
-<<<<<<< Updated upstream
 import model.Components.ComponentStart;
 import view.flowChartComponents.OvalPanel;
-=======
-import model.Memory.OperationType;
->>>>>>> Stashed changes
 import view.flowChartComponents.ParallelogramPanel;
-import view.flowChartComponents.RectanglePanel;
-
 import java.awt.Color;
 
 public class FlowChartContentPanel extends JPanel {
-
 	private static final long serialVersionUID = 1L;
-
 	/**
 	 * Create the panel.
 	 */
@@ -31,29 +21,20 @@ public class FlowChartContentPanel extends JPanel {
 		
 		ComponentOutput output = new ComponentOutput(null, null, null);
 	    output.set("ciao");
-		
+
 		ParallelogramPanel ciao = new ParallelogramPanel(output, null);
+		ciao.setBounds(217, 143, 180, 80);
 		ciao.setBounds(124, 54, 180, 80);
-		
+
 		OvalPanel ok = new OvalPanel(new ComponentStart(null), null);
 		ok.setBounds(0, 144, 180, 80);
-		
+
 		OvalPanel ok2 = new OvalPanel(new ComponentEnd(), null);
 		ok2.setBounds(0, 0, 180, 80);
-		
-		ComponentOperation operation = new ComponentOperation(null, null, null);
-		operation.set("$var1", "$var2", "$var3", OperationType.ADD);
-		
-		RectanglePanel opPanel = new RectanglePanel(operation, null);
-		opPanel.setBounds(217, 250, 180, 80);
-		
+
 		add(ciao);
-<<<<<<< Updated upstream
 		add(ok);
 		add(ok2);
-=======
-		add(opPanel);
->>>>>>> Stashed changes
-	
+
 	}
 }
