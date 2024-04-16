686da43a66fb21dedcce373cf7cd6bc237a61c8d
Sossics <michelerusso267@gmail.com>
2024-04-16 10:44:33 +0200

    Update AlgorithmExecuter.java

--- src/model/AlgorithmExecuter.java
+++ src/model/AlgorithmExecuter.java
@@ -18,18 +18,26 @@ public class AlgorithmExecuter {
 		String result=null;
 		component = start;
 		
+		//Finche il componente non è nullo (arrivati alla fine)
 		while (component!=null) {
 			
 			result=null;
+			
+			//Eseguo il componente
 			result = (String) component.execute();
 			
+			//Controllo se c'è stato un output dal componente
 			if(result!=null) {
 				System.out.println(result);
 				result = null;
 			}
 			
+			//Se il prossimo componente è la fine del programma mostra la tabella della memoria in debug
 			if(component.getNextComponent() instanceof ComponentEnd) MemoryStorage.getInstance().showMemory();
-			component=component.getNextComponent();
+			
+			component=component.getNextComponent(); //Passo all'esecutore il componente successivo
+			
+			//Mostro in fase di debug il passaggio del componente
 			if(component != null)DebuggerConsole.getInstance().printCustomMSGColorLog(referenceType, Color.RED_UNDERLINED, "PASSING EXECUTION TO COMPONENT: " + component.getClass().getSimpleName());
 		}
 		DebuggerConsole.getInstance().printDefaultSuccessLog(referenceType, "Execution completed.");
