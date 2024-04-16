f2967891a86ec21aa39671ae2c2645d8f3327c43
Roman <romanalam8083@gmail.com>
2024-04-16 12:07:37 +0200

    Update FlowChartContentPanel.java

--- src/view/FlowChartContentPanel.java
+++ src/view/FlowChartContentPanel.java
@@ -4,10 +4,17 @@ import javax.swing.JPanel;
 
 import model.Components.ComponentEnd;
 import model.Components.ComponentInput;
+import model.Components.ComponentOperation;
 import model.Components.ComponentOutput;
+<<<<<<< Updated upstream
 import model.Components.ComponentStart;
 import view.flowChartComponents.OvalPanel;
+=======
+import model.Memory.OperationType;
+>>>>>>> Stashed changes
 import view.flowChartComponents.ParallelogramPanel;
+import view.flowChartComponents.RectanglePanel;
+
 import java.awt.Color;
 
 public class FlowChartContentPanel extends JPanel {
@@ -34,9 +41,19 @@ public class FlowChartContentPanel extends JPanel {
 		OvalPanel ok2 = new OvalPanel(new ComponentEnd(), null);
 		ok2.setBounds(0, 0, 180, 80);
 		
+		ComponentOperation operation = new ComponentOperation(null, null, null);
+		operation.set("$var1", "$var2", "$var3", OperationType.ADD);
+		
+		RectanglePanel opPanel = new RectanglePanel(operation, null);
+		opPanel.setBounds(217, 250, 180, 80);
+		
 		add(ciao);
+<<<<<<< Updated upstream
 		add(ok);
 		add(ok2);
+=======
+		add(opPanel);
+>>>>>>> Stashed changes
 	
 	}
 }
