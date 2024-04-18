0471860e74b1ff9be8735a7644975df014816260
Sossics <michelerusso267@gmail.com>
2024-04-18 16:32:54 +0200

    IMPORTANT FIX to FlowChartController

--- src/controller/FlowChartController.java
+++ src/controller/FlowChartController.java
@@ -1,25 +1,43 @@
 package controller;
 
+import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
 
-public class FlowChartController implements ActionListener {
+import javax.swing.JButton;
+import javax.swing.JLabel;
+
+import model.AlgorithmExecuter;
+import model.Timer;
+import model.Memory.MemoryStorage;
+import view.FlowChartContentPanel;
+import view.flowChartComponents.FlowChartPanel;
+import view.flowChartComponents.OvalPanel;
+import view.flowChartComponents.ParallelogramPanel;
+import view.flowChartComponents.RectanglePanel;
+import view.flowChartComponents.RhombusPanel;
+
+public class FlowChartController extends Controller implements ActionListener {
 	
-	private static FlowChartController istance;
-	
-	private FlowChartController() {};
-	
-	public static FlowChartController getIstance() {
-		
-		istance = (istance==null)?new FlowChartController():istance;
-		return istance;
-		
+	public FlowChartController(AlgorithmExecuter ALGORITHM_EXECUTER, Timer TIMER, MemoryStorage MEMORY) {
+		super(ALGORITHM_EXECUTER, TIMER, MEMORY);
+		// TODO Auto-generated constructor stub
 	}
+
+	private static FlowChartController instance;
+	private FlowChartPanel FCPanel;
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		
-		//System.out.println(e.getSource().toString());
+		Object source = e.getSource();
+		if(source instanceof JButton) {
+			JButton button = (JButton) source;
+			FCPanel = (FlowChartPanel) button.getParent();
+		}
+//		System.out.println(e.getSource());
+//		System.out.println(FCPanel);
 		
 		switch (e.getActionCommand()) {
 		case "ADD": {
@@ -30,6 +48,12 @@ public class FlowChartController implements ActionListener {
 		break;
 			}
 		}
+		
+	}
+
+	@Override
+	public void itemStateChanged(ItemEvent e) {
+		// TODO Auto-generated method stub
 		
 	}
 
