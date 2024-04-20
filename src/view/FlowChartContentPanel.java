3dd09af131e5378bb2af0d384908adbfc0e4beb9
Sossics <michelerusso267@gmail.com>
2024-04-20 15:29:41 +0200

    Added dynamic graphic, and updateGraphic method

--- src/view/FlowChartContentPanel.java
+++ src/view/FlowChartContentPanel.java
@@ -4,7 +4,10 @@ import javax.swing.JPanel;
 
 import controller.FlowChartController;
 import model.Components.ComponentAssign;
+import model.DebuggerConsole;
+import model.Components.AlgorithmComponent;
 import model.Components.ComponentComment;
+import model.Components.ComponentDeclaration;
 import model.Components.ComponentEnd;
 import model.Components.ComponentIf;
 import model.Components.ComponentInput;
@@ -23,10 +26,12 @@ import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Rectangle;
+import java.util.ArrayList;
 
 public class FlowChartContentPanel extends JPanel {
 	
 	private static final long serialVersionUID = 1L;
+	private static final String referenceType = "FC-COMP-CONTAINER";
 	private FlowChartController FC_Controller;
 
 	public FlowChartContentPanel() {
@@ -49,45 +54,45 @@ public class FlowChartContentPanel extends JPanel {
 	    output.set("ciao");
 	    
 //	    System.out.println(FC_Controller);
-
-		ParallelogramPanel ciao = new ParallelogramPanel(output, FC_Controller);
-		ciao.setBounds(200, 130, 180, 80);
-
-		OvalPanel ok = new OvalPanel(new ComponentStart(null), FC_Controller);
-		ok.setBounds(20, 20, 180, 80);
-
-		OvalPanel ok2 = new OvalPanel(new ComponentEnd(), FC_Controller);
-		ok2.setBounds(20, 15000, 180, 80);
-		
-		
-		RectanglePanel russo = new RectanglePanel(new ComponentComment(null, null, null), FC_Controller);
-		russo.setBounds(200, 230, 180, 80);
-		
-		ComponentWhile tt = new ComponentWhile(null, null);
-		tt.set("ciao", RelationalOperators.EQUAL_TO, "ciao");
-		RhombusPanel alam = new RhombusPanel(tt, FC_Controller);
-		alam.setBounds(200, 330, 180, 80);
-		alam.setBounds(100, 500, 180, 80);
-		
-		ComponentInput ci = new ComponentInput(null, null, null);
-		ci.set("var");
-		ParallelogramPanel inputPanel = new ParallelogramPanel(ci, FC_Controller);
-		inputPanel.setBounds(100, 700, 180, 80);
-		
-		ComponentOperation co = new ComponentOperation(null, null, null);
-		co.set("$var1", "$var2", "$var3", OperationType.DIV);
-		RectanglePanel operationPanel = new RectanglePanel(co, null);
-		operationPanel.setBounds(100, 900, 180, 80);
-
-		
-		
-		add(ciao);
-		add(ok);
-		add(ok2);
-		add(alam);
-		add(russo);
-		add(inputPanel);
-		add(operationPanel);
+//
+//		ParallelogramPanel ciao = new ParallelogramPanel(output, FC_Controller);
+//		ciao.setBounds(200, 130, 180, 80);
+//
+//		OvalPanel ok = new OvalPanel(new ComponentStart(null), FC_Controller);
+//		ok.setBounds(20, 20, 180, 80);
+//
+//		OvalPanel ok2 = new OvalPanel(new ComponentEnd(), FC_Controller);
+//		ok2.setBounds(20, 15000, 180, 80);
+//		
+//		
+//		RectanglePanel russo = new RectanglePanel(new ComponentComment(null, null, null), FC_Controller);
+//		russo.setBounds(200, 230, 180, 80);
+//		
+//		ComponentWhile tt = new ComponentWhile(null, null);
+//		tt.set("ciao", RelationalOperators.EQUAL_TO, "ciao");
+//		RhombusPanel alam = new RhombusPanel(tt, FC_Controller);
+//		alam.setBounds(200, 330, 180, 80);
+//		alam.setBounds(100, 500, 180, 80);
+//		
+//		ComponentInput ci = new ComponentInput(null, null, null);
+//		ci.set("var");
+//		ParallelogramPanel inputPanel = new ParallelogramPanel(ci, FC_Controller);
+//		inputPanel.setBounds(100, 700, 180, 80);
+//		
+//		ComponentOperation co = new ComponentOperation(null, null, null);
+//		co.set("$var1", "$var2", "$var3", OperationType.DIV);
+//		RectanglePanel operationPanel = new RectanglePanel(co, null);
+//		operationPanel.setBounds(100, 900, 180, 80);
+//
+//		
+//		
+//		add(ciao);
+//		add(ok);
+//		add(ok2);
+//		add(alam);
+//		add(russo);
+//		add(inputPanel);
+//		add(operationPanel);
 	}
 
 	public Dimension getPreferredSize() {
@@ -101,4 +106,35 @@ public class FlowChartContentPanel extends JPanel {
         }   
         return new Dimension(maxX,maxY);
     }
+
+	public void updatePane(ArrayList<AlgorithmComponent> components) {
+		// TODO Auto-generated method stub
+		for(AlgorithmComponent c : components) {
+			DebuggerConsole.getInstance().printDefaultInfoLog(referenceType, "Adding " + c + " to the panel...");
+			if(c instanceof ComponentStart || c instanceof ComponentEnd) {
+				OvalPanel o_p = new OvalPanel(c, FC_Controller);
+				o_p.setBounds(20, 20, 180, 80);
+				add(o_p);
+				DebuggerConsole.getInstance().printDefaultSuccessLog(referenceType, "Added.");
+			}else if(c instanceof ComponentAssign || c instanceof ComponentOperation || c instanceof ComponentComment || c instanceof ComponentDeclaration) {
+				RectanglePanel r_p = new RectanglePanel(c, FC_Controller);
+				r_p.setBounds(200, 230, 180, 80);
+				add(r_p);
+				DebuggerConsole.getInstance().printDefaultSuccessLog(referenceType, "Added.");
+			}else if(c instanceof ComponentWhile || c instanceof ComponentIf) {
+				RhombusPanel rh_p = new RhombusPanel(c, FC_Controller);
+				rh_p.setBounds(100, 500, 180, 80);
+				add(rh_p);
+				DebuggerConsole.getInstance().printDefaultSuccessLog(referenceType, "Added.");
+			}else if(c instanceof ComponentInput || c instanceof ComponentOutput) {
+				ParallelogramPanel par_p = new ParallelogramPanel(c, FC_Controller);
+				par_p.setBounds(200, 130, 180, 80);
+				add(par_p);
+				DebuggerConsole.getInstance().printDefaultSuccessLog(referenceType, "Added.");
+			}
+		}
+		revalidate();
+		repaint();
+		
+	}
 }
