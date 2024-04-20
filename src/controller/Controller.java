f7e1df39ece41f24785f5d03c64edf0409fb72ba
Sossics <michelerusso267@gmail.com>
2024-04-20 16:09:01 +0200

    Added implementation to Controller (MouseListener)

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -4,12 +4,13 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.awt.event.MouseListener;
 
 import model.AlgorithmExecuter;
 import model.Timer;
 import model.Memory.MemoryStorage;
 
-public abstract class Controller implements ActionListener, ItemListener {
+public abstract class Controller implements ActionListener, ItemListener, MouseListener {
 	
 	protected AlgorithmExecuter algorithmExecuter;
 	protected MemoryStorage memory;
