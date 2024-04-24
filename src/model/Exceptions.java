a055f839a5ba5c61283bd7f676fbdcb91ec3e219
Sossics <michelerusso267@gmail.com>
2024-04-24 20:40:02 +0200

    Added new Exception

--- src/model/Exceptions.java
+++ src/model/Exceptions.java
@@ -8,6 +8,7 @@ public class Exceptions extends Exception{
 	public static final String EXISTING_VARIABLE = "The variable already exists, choose another name";
 	public static final String NON_EXISTING_VARIABLE = "The variable does not exists, try with another name";
 	public static final String UNMATCH_TYPE = "The variable type and the type of the value/variable aren't the same!";
+	public static final String INVALID_CAST = "The variable type and the type of the value aren't the same!";
 	public static final String UNKNOWN_TYPE = "Unknown variable type!";
 	public static final String CONVERSION_ERROR = "The value cannot be converted";
 	public static final String STRING_ERROR = "Strings don't support this operation!";
