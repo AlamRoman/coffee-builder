026ca66d8d7c98300c1bf5f8c461364694934596
Sossics <michelerusso267@gmail.com>
2024-04-24 20:40:37 +0200

    Fixed panel configuration

--- src/view/editComponents/EditInput.java
+++ src/view/editComponents/EditInput.java
@@ -37,7 +37,7 @@ public class EditInput extends EditComponent{
 		setSize(335, 130);
 		
 		JButton btnSave = new JButton("Save");
-		btnSave.setBounds(0, 75, 323, 19);
+		btnSave.setBounds(0, 72, 323, 19);
 		getContentPane().add(btnSave);
 		
 		btnSave.addActionListener(new ActionListener() {
@@ -52,17 +52,17 @@ public class EditInput extends EditComponent{
 		});
 		
 		textFieldVarName = new JTextField();
-		textFieldVarName.setBounds(94, 51, 122, 17);
+		textFieldVarName.setBounds(103, 52, 122, 17);
 		getContentPane().add(textFieldVarName);
 		textFieldVarName.setColumns(10);
 		
 		lblNewLabel = new JLabel("$");
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 14));
 		lblNewLabel.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblNewLabel.setBounds(51, 54, 40, 11);
+		lblNewLabel.setBounds(60, 54, 40, 11);
 		getContentPane().add(lblNewLabel);
 		
-		lblNewLabel_1 = new JLabel("Inserisci il nome della variabile da prendere in input : ");
+		lblNewLabel_1 = new JLabel("Insert the variable:");
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel_1.setBounds(0, 31, 323, 11);
 		getContentPane().add(lblNewLabel_1);
