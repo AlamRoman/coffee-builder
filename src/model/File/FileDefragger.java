4bdd2057996d59e9c9c290450e2c4b8f9ec1119e
Sossics <michelerusso267@gmail.com>
2024-04-25 11:25:48 +0200

    FileDefragger Updated

--- src/model/File/FileDefragger.java
+++ src/model/File/FileDefragger.java
@@ -9,7 +9,21 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
+import model.Components.AlgorithmComponent;
+import model.Components.ComponentAdd;
+import model.Components.ComponentAssign;
+import model.Components.ComponentComment;
+import model.Components.ComponentDeclaration;
+import model.Components.ComponentElse;
+import model.Components.ComponentEnd;
+import model.Components.ComponentIf;
+import model.Components.ComponentInput;
+import model.Components.ComponentOperation;
+import model.Components.ComponentOutput;
+import model.Components.ComponentStart;
+import model.Components.ComponentWhile;
 import model.Memory.MemoryStorage;
+import model.Memory.RelationalOperators;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
@@ -26,11 +40,10 @@ public class FileDefragger {
     private static final String FILE_EXTENSION = ".cbalg";
     private static JFrame frame;
     
-    public static void openFile(MemoryStorage memory, JFrame parentFrame) {
+    public static void openFile(JFrame parentFrame) {
     	
     	frame = parentFrame;
     	
-        // Mostra il selettore di file
         JFileChooser fileChooser = new JFileChooser();
         fileChooser.setDialogTitle("Select .cbalg File to Decrypt");
         
@@ -64,42 +77,174 @@ public class FileDefragger {
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-
-                // Stampare gli arraylist per verificare il risultato
-                for (String[] array : arrayList) {
-                    for (String element : array) {
-                        System.out.print(element + " ");
-                    }
-                    System.out.println();
+                
+                ArrayList<AlgorithmComponent> newArrayComponents = new ArrayList<AlgorithmComponent>(arrayList.size());
+                for (int i = 0; i < arrayList.size(); i++) {
+                    newArrayComponents.add(null);
                 }
+                
+                for (int i = arrayList.size() - 1; i >= 0; i--) {
+                	elaborateComponentInfoAndAdd(arrayList, i, newArrayComponents, MemoryStorage.getInstance());
+                }
+                
+                System.out.println(newArrayComponents.toString());
+                
+                MemoryStorage.getInstance().setComponents(newArrayComponents);
+            
             }
-            
-            
         }
     }
 
-    private static boolean isOnCVolume(File directory) {
-        // Ottieni il percorso del volume C
+    // C, N1, NIDX1, N2, NIDX2, ATTR1, ATTR2, ATTR3, ATTR4
+    private static void elaborateComponentInfoAndAdd(ArrayList<String[]> arrayList, int i, ArrayList<AlgorithmComponent> newArrayComponents, MemoryStorage instance) {
+		// TODO Auto-generated method stub
+    	Integer N1_idx = null;
+    	Integer N2_idx = 0;
+    	
+    	AlgorithmComponent aus = null;
+    	
+    	try {
+    		N1_idx = Integer.parseInt(arrayList.get(i)[2]);
+    		if(N1_idx==-1) N1_idx = 0;
+    	} catch (Exception e) {
+    		// TODO: handle exception
+    		
+    	}
+    	
+    	try {
+    		N2_idx = Integer.parseInt(arrayList.get(i)[4]);
+    		if(N2_idx==-1) N2_idx = 0;
+    	} catch (Exception e) {
+    		// TODO: handle exception
+    	}
+    	
+    	switch (arrayList.get(i)[0]) {
+			case "CS":
+				ComponentStart temp_start = (ComponentStart) aus;
+				temp_start = new ComponentStart(newArrayComponents.get(N1_idx));
+				aus = temp_start;
+				break;
+			case "CA":
+				ComponentAssign temp_assign = (ComponentAssign) aus;
+				temp_assign = new ComponentAssign(newArrayComponents.get(N1_idx), newArrayComponents.get(N2_idx), instance);
+				temp_assign.set(arrayList.get(i)[5], arrayList.get(i)[6]);
+				break;
+			case "CD":
+				ComponentDeclaration temp_declaration = (ComponentDeclaration) aus;
+				aus = new ComponentDeclaration(newArrayComponents.get(N1_idx), newArrayComponents.get(N2_idx), instance);
+				break;
+			case "CO":
+				ComponentOperation temp_operation = (ComponentOperation) aus;
+				aus = new ComponentOperation(newArrayComponents.get(N1_idx), newArrayComponents.get(N2_idx), instance);
+				break;
+			case "CIF":
+				ComponentIf temp_if = (ComponentIf) aus;
+				aus = new ComponentIf(newArrayComponents.get(N1_idx), newArrayComponents.get(N2_idx), instance);
+				break;
+			case "CW":
+				ComponentWhile temp_while = (ComponentWhile) aus;
+				if(newArrayComponents.get(i) == null){
+					temp_while = new ComponentWhile(newArrayComponents.get(N2_idx), instance);
+				}else {
+					temp_while.set(arrayList.get(i)[5], getRelationalOperator(arrayList.get(i)[7]), arrayList.get(i)[6]);
+					temp_while.setNextComponent1(newArrayComponents.get(N1_idx));
+					temp_while.setNextComponent2(newArrayComponents.get(N2_idx));
+				}
+				aus = temp_while;
+//				aus = new ComponentWhile(newArrayComponents.get(N1_idx), instance);
+				break;
+			case "CE":
+				ComponentElse temp_else = (ComponentElse) aus;
+				aus = new ComponentElse(newArrayComponents.get(N1_idx), newArrayComponents.get(N2_idx), instance);
+				break;
+			case "CADD":
+				ComponentAdd temp_add = (ComponentAdd) aus;
+				aus = new ComponentAdd(newArrayComponents.get(N1_idx), newArrayComponents.get(N2_idx), instance);
+				break;
+			case "CCOM":
+				ComponentComment temp_comment = (ComponentComment) aus;
+				aus = new ComponentComment(newArrayComponents.get(N1_idx), newArrayComponents.get(N2_idx), instance);
+				break;
+			case "CI":
+				ComponentInput temp_input = (ComponentInput) aus;
+				aus = new ComponentInput(newArrayComponents.get(N1_idx), newArrayComponents.get(N2_idx), instance);
+				break;
+			case "COUT":
+				ComponentOutput temp_output = (ComponentOutput) aus;
+				aus = new ComponentOutput(newArrayComponents.get(N1_idx), newArrayComponents.get(N2_idx), instance);
+				break;
+			case "CEND":
+				ComponentEnd temp_end = (ComponentEnd) aus;
+				aus = new ComponentEnd();
+				break;
+    	}
+    	if(!arrayList.get(i)[0].equals("CEND")) {
+    		if(aus.getNextComponent1() == null && i != N1_idx) {
+    			newArrayComponents.set(N1_idx, new ComponentWhile(null, null));
+    			aus.setNextComponent1(newArrayComponents.get(N1_idx));
+    		}else if(aus.getNextComponent1() == null && i == N1_idx) {
+    			aus.setNextComponent1(aus);
+    		}    		
+    	}
+    	
+    	newArrayComponents.set(i, aus);
+		
+	}
+
+	private static RelationalOperators getRelationalOperator(String string) {
+		// TODO Auto-generated method stub
+		switch (string) {
+			case "EQUAL_TO": {
+				return RelationalOperators.EQUAL_TO;
+			}
+			case "GREATER_THAN": {
+				return RelationalOperators.GREATER_THAN;
+			}
+			case "LESS_THAN": {
+				return RelationalOperators.LESS_THAN;
+			}
+			case "NOT_EQUAL_TO": {
+				return RelationalOperators.NOT_EQUAL_TO;
+			}
+			case "GREATER_THAN_EQUAL_TO": {
+				return RelationalOperators.GREATER_THAN_EQUAL_TO;
+			}
+			case "NOT": {
+				return RelationalOperators.NOT;
+			}
+			case "LESS_THAN_EQUAL_TO": {
+				return RelationalOperators.LESS_THAN_EQUAL_TO;
+			}
+			default:
+				return RelationalOperators.EQUAL_TO;
+		}
+	}
+
+	private static boolean isOnCVolume(File directory) {
+        // Gets the path of System volume
         String cVolumePath = System.getenv("SystemDrive") + "\\";
-        // Controlla se il percorso della directory inizia con il percorso del volume C
+        // Returns true if the selected file is in the system volume, false if its not
         return directory.getAbsolutePath().startsWith(cVolumePath);
     }
 
 	private static File decryptAndSave(File inputFile) {
         try {
-            // Leggi il contenuto del file cifrato
+        	
+            // Read the encrypted data from file
             byte[] encryptedData = Files.readAllBytes(Paths.get(inputFile.getAbsolutePath()));
             
+            // Save the iv from file
             byte[] iv = Arrays.copyOfRange(encryptedData, 0, 16);
             
+            // Save only the content of the file
             byte[] encryptedContent = Arrays.copyOfRange(encryptedData, 16, encryptedData.length);
             
-            // Decifra il contenuto
+            // Decrypt the content with provided iv and AES Key
             byte[] decryptedData = decrypt(encryptedContent, AES_KEY_STRING.getBytes(StandardCharsets.UTF_8), iv);
             
-            // Crea un file temporaneo con estensione .txt
+            // Create a temp file .txt
             File tempFile = new File(inputFile.getParent(), "tempCBALG_decrypted.txt");
-            // Salva il contenuto decifrato nel file temporaneo
+            // Save the decrypted content in the temp file
             Files.write(tempFile.toPath(), decryptedData);
             
             System.out.println("Decrypted content saved to: " + tempFile.getAbsolutePath());
